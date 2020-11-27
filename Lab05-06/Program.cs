986c32a4a893eb74e8d47fcb49f1188ea54dfa8c
StatTrak <wireless.00.d@gmail.com>
2020-11-27 11:32:49 +0200

    Добавьте файлы проекта.

---
+++ Lab05-06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Printer1;
+
+namespace Lab05_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = PrinterPropertis.PrinterInit();
+            printer.outPrinter();
+            Console.ReadKey();
+        }
+    }
+}
